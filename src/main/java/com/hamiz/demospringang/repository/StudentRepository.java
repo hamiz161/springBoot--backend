998579320112de22bc6724a2028089a8dca232d0
hamiz161 <hamiz_18@hotmail.com>
2024-05-17 22:04:16 -0400

    Spring-angular fullStack

--- src/main/java/com/hamiz/demospringang/repository/StudentRepository.java
+++ src/main/java/com/hamiz/demospringang/repository/StudentRepository.java
@@ -8,7 +8,7 @@ import java.util.List;
 public interface StudentRepository extends JpaRepository<Student,String> {
 
     Student findBytCode(String code);
-    List<Student> findByStatus(String programId);
+    List<Student> findByProgramId(String programId);
 
 
 }
